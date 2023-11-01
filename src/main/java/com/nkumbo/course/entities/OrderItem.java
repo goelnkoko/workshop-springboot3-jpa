cfef0ed2c060c06f6fb88aa33141f54c9dfcdfbf
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-01 22:55:12 +0100

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/nkumbo/course/entities/OrderItem.java
@@ -0,0 +1,73 @@
+package com.nkumbo.course.entities;
+
+import com.nkumbo.course.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantitiy;
+    private Double price;
+    public OrderItem(){}
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        setProduct(product);
+    }
+
+    public OrderItem(Order order, Product product, Integer quantitiy, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantitiy = quantitiy;
+        this.price = price;
+    }
+
+    public Integer getQuantitiy() {
+        return quantitiy;
+    }
+
+    public void setQuantitiy(Integer quantitiy) {
+        this.quantitiy = quantitiy;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
