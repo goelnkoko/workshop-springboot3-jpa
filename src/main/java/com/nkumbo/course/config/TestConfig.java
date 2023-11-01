011327770f7cc30f6915f8efa4f733b6941665be
Goel Nkoko <goelnkoko12@hotmail.com>
2023-11-01 01:04:12 +0100

    Category

--- src/main/java/com/nkumbo/course/config/TestConfig.java
+++ src/main/java/com/nkumbo/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.nkumbo.course.config;
 
+import com.nkumbo.course.entities.Category;
 import com.nkumbo.course.entities.Order;
 import com.nkumbo.course.entities.User;
 import com.nkumbo.course.entities.enums.OrderStatus;
+import com.nkumbo.course.repositories.CategoryRepository;
 import com.nkumbo.course.repositories.OrderRepository;
 import com.nkumbo.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
